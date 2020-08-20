31a7c3d7cc39c9f67e24dee8dbf9e49e86634577
Phani Varma Mandapati <pmandapati@NextSphere.com>
2020-08-21 03:46:32 +0530

    New

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8020;
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/project353'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/project353/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8020);
+app.listen(port);
