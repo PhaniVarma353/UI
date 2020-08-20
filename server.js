2d7717e32292b585ea7fa28f0c6f0d9fb97deac4
Phani Varma Mandapati <pmandapati@NextSphere.com>
2020-08-21 01:52:28 +0530

    Commit

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/project353/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8070);
