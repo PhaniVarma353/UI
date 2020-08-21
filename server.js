e4b2d4ff3ecccc0e9e2f340db6381e2fb26f189c
Phani Varma Mandapati <pmandapati@NextSphere.com>
2020-08-21 23:59:24 +0530

    Server updtae

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/project353'));
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/project353/index.html'));
+// });
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080); 
+
+
 const express = require('express');
+const http = require('http')
 const path = require('path');
+
 const app = express();
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/project353'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/project353/index.html'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); 
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
